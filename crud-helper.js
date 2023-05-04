d7ca8294c0e1cd9383013954e79a551eca7c151d
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-05-04 16:10:11 -0400

    Corrected a typo

--- crud-helper.js
+++ crud-helper.js
@@ -1,11 +1,10 @@
 // Connect to the database
 require("dotenv").config();
 require("./config/database");
-const User = require("./models/user");
 
 // Require the Mongoose models
-// const User = require('./models/user');
-// const Item = require('./models/item');
+const User = require("./models/user");
+const Item = require("./models/item");
 // const Category = require('./models/category');
 // const Order = require('./models/order');
 
