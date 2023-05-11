aaf41e958d9fe3e8a6f1f6f387721e5b82e3b14a
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-05-10 20:45:30 -0400

    Changed the scale of the sign up and log in fields to make it fit in the page.

--- src/components/LogInForm/LogInForm.jsx
+++ src/components/LogInForm/LogInForm.jsx
@@ -31,7 +31,7 @@ export default function LogInForm({ setUser }) {
           position: "absolute",
           top: "50%",
           left: "50%",
-          transform: "translate(30%, 36%) scale(1.4)",
+          transform: "translate(30%, 36%)",
         }}
       >
         <div className="form-container">
